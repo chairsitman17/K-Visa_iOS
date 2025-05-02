780eca1ae700860241c0422fda5eac966be5577b
Ian Jeong <chairsitman17@gmail.com>
2025-05-02 12:12:43 +0900

    Initial Commit

---
+++ K-Visa/K_VisaApp.swift
@@ -0,0 +1,17 @@
+//
+//  K_VisaApp.swift
+//  K-Visa
+//
+//  Created by 정유석 on 5/2/25.
+//
+
+import SwiftUI
+
+@main
+struct K_VisaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ K-VisaTests/K_VisaTests.swift
@@ -0,0 +1,17 @@
+//
+//  K_VisaTests.swift
+//  K-VisaTests
+//
+//  Created by 정유석 on 5/2/25.
+//
+
+import Testing
+@testable import K_Visa
+
+struct K_VisaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
